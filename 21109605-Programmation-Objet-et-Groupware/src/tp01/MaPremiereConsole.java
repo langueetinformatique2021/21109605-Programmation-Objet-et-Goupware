26c8776b999b384825cb5c72ae1021920fd62e1c
user <user@DESKTOP-BMGRPDS>
2021-11-02 18:23:02 +0100

    tp3

---
+++ 21109605-Programmation-Objet-et-Groupware/src/tp01/MaPremiereConsole.java
@@ -0,0 +1,14 @@
+package tp01;
+
+import util.Console;
+
+public class MaPremiereConsole {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Console c = new Console();
+			c.println("Claude");
+			c.println("Montacié");
+	}
+
+}
---
+++ 21109605-Programmation-Objet-et-Groupware/src/tp01/SimpleProgramme2.java
@@ -0,0 +1,11 @@
+package tp01;
+
+public class SimpleProgramme2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.print("Berthilde ");
+			System.out.print("Biard");
+	}
+
+}
